bfeeb3c8bfa82ee2ed14de5dc2a702fcd4ec3dcb
Girein <giovanni.reinard@gmail.com>
2021-08-12 20:48:59 +0700

    add Paginator attribute

--- JsonResponse.go
+++ JsonResponse.go
@@ -2,10 +2,11 @@ package pretty
 
 // JSONResponse format
 type JSONResponse struct {
-	StatusCode string      `json:"status_code"`
-	StatusText string      `json:"status_text"`
-	Message    string      `json:"message"`
-	Data       interface{} `json:"data"`
+	StatusCode string         `json:"status_code"`
+	StatusText string         `json:"status_text"`
+	Message    string         `json:"message"`
+	Data       interface{}    `json:"data"`
+	Paginator  map[string]int `json:"paginator"`
 }
 
 // Simple Response
@@ -21,4 +22,18 @@ func (r *JSONResponse) WithData(statusCode string, statusText string, message st
 	r.StatusText = statusText
 	r.Message = message
 	r.Data = data
+}
+
+// WithData Response
+func (r *JSONResponse) WithPagination(statusCode string, statusText string, message string, data interface{}, paginator map[string]int) {
+	r.StatusCode = statusCode
+	r.StatusText = statusText
+	r.Message = message
+	r.Data = data
+	r.Paginator = map[string]int{
+		"per_page":      paginator["PageSize"],
+		"current_page":  paginator["Page"],
+		"previous_page": paginator["Page"] - 1,
+		"next_page":     paginator["Page"] + 1,
+	}
 }
